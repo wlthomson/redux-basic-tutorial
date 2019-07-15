72d1dcd15ae513d9d40f8c0223cc016304e33538
Will Thomson <will.tho@outlook.com>
2019-07-16 01:46:48 +1200

    Add actions

---
+++ actions.js
@@ -0,0 +1,37 @@
+/*
+ * Action types.
+ */
+
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+/*
+ * Other constants.
+ */
+
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
+
+/*
+ * Action creators.
+ */
+
+export const addTodo = text => ({ type: ADD_TODO, text });
+export const toggleTodo = index => ({ type: TOGGLE_TODO, index });
+export const setVisibilityFilter = filter => ({
+  type: SET_VISIBILITY_FILTER,
+  filter
+});
+
+/*
+ * Bound action creators.
+ */
+
+export const boundAddTodo = text => dispatch(addTodo(text));
+export const boundToggleTodo = index => dispatch(toggleTodo(index));
+export const boundSetVisibilityFilter = filter =>
+  dispatch(setVisibilityFilter(filter));
