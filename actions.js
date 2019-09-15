a11f172105fd3d5248b5a6c730388592094d4ba4
Will Thomson <will.tho@outlook.com>
2019-09-15 12:51:00 +1200

    Add reducers

--- actions.js
+++ actions.js
@@ -10,7 +10,7 @@ export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
  * Other constants.
  */
 
-export const VisibilityFilters = {
+export const VISIBILITY_FILTERS = {
   SHOW_ALL: "SHOW_ALL",
   SHOW_COMPLETED: "SHOW_COMPLETED",
   SHOW_ACTIVE: "SHOW_ACTIVE"
